bb22ddd451dd7de34a32a2d82b4c7028783b86ed
Joshua Swanson <swanson6@uw.edu>
2023-03-27 15:31:09 -0700

    improvements to Generic class

--- instance.py
+++ instance.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-def sphere(K,d):
+def sphere(K, d):
     '''
-    K: arms
-    d: dimensions
+    Generates arms and optimal arm of a unit sphere instance.
+    K: int, number of arms
+    d: int, dimension of each arm
+    Returns:
+    - X: numpy array, matrix of K arms of dimension d.
+    - theta_star: numpy array, 1 x d vector representing the optimal arm.
     '''
-    X = np.random.randn(K, d)/np.sqrt(d)  # arms
+    X = np.random.randn(K, d)/np.sqrt(d)
     norms = np.linalg.norm(X, axis=1).reshape(K, 1)
     X /= norms
     theta_star = np.random.randn(d)
@@ -13,9 +17,13 @@ def sphere(K,d):
     return X, theta_star
 
 def soare(d, alpha):
-    '''    
-    d: dimensions
-    alpha: angle
+    '''
+    Generates arms and optimal arm of a "Soare" instance.
+    d: int, dimension of each arm
+    alpha: float, 
+    Returns:
+    - X: numpy array, matrix of d+1 arms of dimension d.
+    - 2*e_1: numpy array, 1 x d vector representing the optimal arm.
     '''
     X = np.eye(d)
     e_1, e_2 = X[:2]
